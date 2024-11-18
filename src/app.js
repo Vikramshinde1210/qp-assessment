4c90668cc533e09b91e7271ae439900cef72a504
vikramshinde1210 <shindevikram1210@gmail.com>
2024-11-18 18:49:47 +0530

    feat: separate db layer from app

--- src/app.js
+++ src/app.js
@@ -1,20 +1,6 @@
+require('./utils/envloader.js');
 const express = require('express');
-const dotenv = require('dotenv')
-const path = require('path')
-const { Sequelize } = require('sequelize');
-
-dotenv.config({ path: `${path.resolve(__dirname, "../.env")}`})
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mysql',
-  }
-);
+const sequelize = require('./database');
 
 const app = express();
 app.use(express.json());
