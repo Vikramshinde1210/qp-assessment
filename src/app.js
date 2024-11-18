1876cd47c5b49372a178aa4ca030df136a6f08e9
vikramshinde1210 <shindevikram1210@gmail.com>
2024-11-18 18:37:43 +0530

    feat: setup db connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const path = require('path')
+const { Sequelize } = require('sequelize');
 
 dotenv.config({ path: `${path.resolve(__dirname, "../.env")}`})
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+  }
+);
 
 const app = express();
 app.use(express.json());
@@ -20,7 +32,10 @@ app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({ success: false, message: err.message || 'Error From Server' });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+sequelize.sync().then(() => {
+  console.log('Database synced');
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
 });
 
